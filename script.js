1bad8ca70f1e57f445bbed4b5271e30039ab3651
Ítalo Conceição <56076361+italoconceicao@users.noreply.github.com>
2024-04-26 16:35:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,79 @@
-let fileInput = document.querySelector('#arquivo');
-let output = document.querySelector('#texto');
-var conteudoFormatado = []
+body {
+    border: 0px; margin: 0px; padding: 0px;
+    background-color: #04AA6D;
+}
 
-fileInput.addEventListener('change', async () => {
-    const [file] = fileInput.files;
+header {
+    height: 10vh;
+    width: 100vw;
+    text-align: center;
+}
     
-    if (file) {
-        conteudo = await file.text();
-        output.innerText = conteudo;
-    }
+#container-corpo {
+    margin: auto;
+    padding-top: 10vh;
+    height: 50vh;
+    width: 90%;
+    display: flex;
+    align-items: baseline;
+    justify-content: center;
+}
+
+#div1 {   
+    width: 30%;
+    height: 30vh;
+    text-decoration: none;
+}
+
+input {
+    display: none;
+    cursor: pointer;
+}
+
+label {
+	box-shadow: 0px 10px 10px -9px rgb(66, 66, 66);
+	background-color: lightgreen;
+	border-radius:11px;
+	cursor:pointer;
+	color:#04AA6D;
+	font-family:Trebuchet MS;
+	font-size:20px;
+	font-weight:bold;
+	padding:15px 9px;
+	text-decoration:none;
+}
+
+label:hover {
+	background-color:rgb(97, 226, 97);
+}
+
+label:active {
+	position:relative;
+	top:1px;
+}
+
+#div2 {
+    width: 30%;
+    height: 30vh;
     
-    conteudoFormatado = conteudo.split(' ');
-});
+}
+
+#div2 p{
+    color: white;
+    font-size: x-large;
+}
+
+textarea {
+    width: 100%;
+    height: 100%;
+}
+
+#div2 button {
+    width: 30%;
+    height: 20%;
+    cursor: pointer;
+}
+
+#resultado p {
+    text-align: center;
+}
