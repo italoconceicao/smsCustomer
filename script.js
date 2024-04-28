615a8108c49929abd3c64f6329111bd4bd035152
Ítalo Conceição <56076361+italoconceicao@users.noreply.github.com>
2024-04-28 14:16:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,58 +5,53 @@ let output = document.querySelector('#texto');
 let botaoEnviar = document.querySelector('#botaoEnviar')
 let resultado = document.querySelector('#textoResultado')
 
-
 fileInput.addEventListener('change', async () => {
+  const [file] = fileInput.files;
     
-    const [file] = fileInput.files;
-    
-    if (file) {
+  if (file) {
         conteudo = await file.text();
         output.innerText = conteudo;
-    }
+  }
     
-    console.log(conteudo); // <= CONFERENCIA
-    let outputValue = document.querySelector('#texto').value;
-    conteudoFormatado = outputValue.split(' ')
-
-
-    console.log(conteudoFormatado) // <= CONFERENCIA
+  let outputValue = document.querySelector('#texto').value;
+  conteudoFormatado = outputValue.split(' ')
 });
 
-botaoEnviar.addEventListener('click', () => {
+function hangleGetFormValues() {
+  const whatsapp = ["555133711447"] // <========
+  const mensagem = `${conteudoFormatado[1]} ${conteudoFormatado[7]} ${conteudoFormatado[8]} ${conteudoFormatado[15]}`;
+  
+  return {
+    mensagem,
+    whatsapp,
+  }
+}
 
-  resultado.innerText = `${conteudoFormatado[1]} ${conteudoFormatado[7]} ${conteudoFormatado[8]} ${conteudoFormatado[15]}`;
+
+async function dispararMensagem(mensagem, whatsapp){
+
+  const GZAPPY_URL = "https://api.gzappy.com/v1/message/send-message"
+
+  const response = await fetch(GZAPPY_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      "user_token_id": '4968d164-15aa-4e1b-9d87-7d977fd1077a'
+    },
+    body: JSON.stringify({
+      instance_id: 'F78KTNMGJRMWYWBFDX23FLYY',
+      instance_token: '7d941d6a-bf42-48f1-ad40-992d5029d824',
+      message: mensagem,
+      phone: whatsapp,
+    })
+  })
+
+  const data = await response.json() // <====== REVISAR
+  console.log(data)
+  { msg: 'Messages sent' }
+}
+
+botaoEnviar.addEventListener('click', async () => {
+  const dados = hangleGetFormValues();
+  await dispararMensagem(dados.mensagem, dados.whatsapp)
 })
-
-
-
-
-
-
-// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-// const destinatario = null;
-
-// const GZAPPY_URL = "https://api.gzappy.com/v1/message/send-message"
-
-// const response = await fetch(GZAPPY_URL, {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     "user_token_id": '$USER_TOKEN_ID'
-//   },
-//   body: JSON.stringify({
-//     instance_id: '$INSTANCE_ID',
-//     instance_token: '$INSTANCE_TOKEN',
-//     message: [`Firmeza, irmão?!
-// chegou ao nosso conhecimento que voce ta em débito com a Caixa Ecônomica, ta ligado
-// e acontece que ficou feio pra tu
-// e seguinte, tu vai te que ta fazendo uma pix pra gente pra gente ta resolvendo isso ae
-//     `],
-//     phone: ["5551982616617"]
-//   })
-// })
-
-// const data = await response.json()
-// console.log(data)
-// { msg: 'Messages sent' }
